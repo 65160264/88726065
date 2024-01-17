8193a253491565355982d47f2bda85fca75bdc2a
65160264 <65160264@go.buu.ac.th>
2024-01-17 19:30:17 +0700

    ลบรายการ Todo

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.getElementById("todo-list");
     const todoInput = document.getElementById("todo-input");
     const addButton = document.getElementById("add-button");
+    // อาร์เรย์ส าหรับเก็บรายการ Todo
     let todos = [];
+    // เพิ่มรายการ Todo
     function addTodo() {
         const todoText = todoInput.value.trim();
         if (todoText !== "") {
@@ -16,41 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
             todoInput.value = "";
         }
     }
+    // ลบรายการ Todo
     function deleteTodo(index) {
         todos.splice(index, 1);
         renderTodoList();
     }
-    function toggleComplete(index) {
-        todos[index].completed = !todos[index].completed;
-        renderTodoList();
-    }
-    function renderTodoList() {
-        console.log(todos);
-        todoList.innerHTML = "";
-        for (let i = 0; i < todos.length; i++) {
-            const todoItem = todos[i];
-            const listItem = document.createElement("li");
-            listItem.textContent = todoItem.text;
-            if (todoItem.completed) {
-                listItem.classList.add("completed");
-            }
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "ลบ";
-            deleteButton.addEventListener("click", () => deleteTodo(i));
-            const completeButton = document.createElement("button");
-            completeButton.textContent = todoItem.completed ? "ยกเลิก" : "เสร็จ";
-            completeButton.addEventListener("click", () => toggleComplete(i));
-            listItem.appendChild(completeButton);
-            listItem.appendChild(deleteButton);
-            todoList.appendChild(listItem);
-        }
-    }
-    addButton.addEventListener("click", addTodo);
-    todoInput.addEventListener("keypress", function (event) {
-        if (event.key === "Enter") {
-            addTodo();
-        }
-    });
+    
 
+    // แสดงรายการ Todo คร้ังแรก
     renderTodoList();
 });
